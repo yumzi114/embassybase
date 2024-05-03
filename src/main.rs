438306107034178c195be7db37013c429176e90d
yumzi114 <yumzi114@gmail.com>
2024-05-03 15:19:54 +0900

    asd

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,43 @@
 #![no_std]
 #![no_main]
 #![feature(type_alias_impl_trait)]
+
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
 use esp32c3_hal::{clock::ClockControl, embassy, peripherals::Peripherals, prelude::*};
 use esp_backtrace as _;
 use esp_println::print;
 
+#[embassy_executor::task]
+async fn one_second_task() {
+    let mut count = 0;
+    loop {
+        esp_println::println!("Spawn Task Count: {}", count);
+        count += 1;
+        Timer::after(Duration::from_millis(1_000)).await;
+    }
+}
+
 #[main]
 async fn main(spawner: Spawner) {
-    // println!("Hello, world!");
+    let peripherals = Peripherals::take();
+    let system = peripherals.SYSTEM.split();
+    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
+
+    embassy::init(
+        &clocks,
+        esp32c3_hal::timer::TimerGroup::new(peripherals.TIMG0, &clocks).timer0,
+    );
+
+    spawner.spawn(one_second_task()).unwrap();
+
+    // This line is for Wokwi only so that the console output is formatted correctly
+    print!("\x1b[20h");
+
+    let mut count = 0;
+    loop {
+        esp_println::println!("Main Task Count: {}", count);
+        count += 1;
+        Timer::after(Duration::from_millis(5_000)).await;
+    }
 }
